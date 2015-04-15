8fb42052bbf877d13cbe94fd4c1a439440f08fda
Mario Basic <mario.basic@outlook.com>
2015-04-16 00:42:35 +0200

    Fixed redirect to github route.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,8 +11,11 @@
 |
 */
 
+use Illuminate\Http\Response;
+
 $app->get('/', function() {
-    return redirect('https://github.com/laravelista/Lissandra');
+    return response('Redirecting to Repository on GitHub...', Response::HTTP_TEMPORARY_REDIRECT)
+        ->header('location', 'https://github.com/laravelista/Lissandra');
 });
 
 $app->get('api/v1/feed/lessons', 'App\Http\Controllers\FeedController@lessons');
